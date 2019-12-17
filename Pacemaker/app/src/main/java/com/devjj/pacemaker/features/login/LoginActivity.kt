6dc042bb9162036b80b0d885f3904eb26fa0b6cd
leejh <timing.uniq@gmail.com>
2019-12-17 17:29:54 +0900

    191217 additionActivity()

--- Pacemaker/app/src/main/java/com/devjj/pacemaker/features/login/LoginActivity.kt
+++ Pacemaker/app/src/main/java/com/devjj/pacemaker/features/login/LoginActivity.kt
@@ -2,9 +2,13 @@ package com.devjj.pacemaker.features.login
 
 import android.content.Context
 import android.content.Intent
+import com.devjj.pacemaker.R
 import com.devjj.pacemaker.core.platform.BaseActivity
 
 class LoginActivity : BaseActivity() {
+
+    override var layout = R.layout.activity_layout
+    override var fragmentId = R.id.fragmentContainer
 
     companion object {
         fun callingIntent(context: Context) = Intent(context, LoginActivity::class.java)
---
+++ Pacemaker/app/src/main/java/com/devjj/pacemaker/features/pacemaker/addition/AdditionViewModel.kt
@@ -0,0 +1,10 @@
+package com.devjj.pacemaker.features.pacemaker.addition
+
+import androidx.lifecycle.ViewModel
+import com.devjj.pacemaker.core.platform.BaseViewModel
+import javax.inject.Inject
+
+class AdditionViewModel
+@Inject constructor() : BaseViewModel() {
+
+}

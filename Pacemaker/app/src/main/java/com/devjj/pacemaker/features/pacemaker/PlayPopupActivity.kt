3d68ca88c863f152fcbcaa31060251730859ce01
JYP <inty.coffee@gmail.com>
2019-12-25 00:08:07 +0900

    19-12-25 popup layout

--- Pacemaker/app/src/main/java/com/devjj/pacemaker/features/pacemaker/PlayPopupActivity.kt
+++ Pacemaker/app/src/main/java/com/devjj/pacemaker/features/pacemaker/PlayPopupActivity.kt
@@ -2,13 +2,18 @@ package com.devjj.pacemaker.features.pacemaker
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.Window
+import androidx.recyclerview.widget.RecyclerView
 import com.devjj.pacemaker.R
 import com.devjj.pacemaker.core.navigation.Navigator
 import com.devjj.pacemaker.core.platform.BaseActivity
 import com.devjj.pacemaker.features.pacemaker.play.PlayPopupFragment
 import javax.inject.Inject
+import kotlin.math.roundToInt
 
 class PlayPopupActivity : BaseActivity() {
 
@@ -24,7 +29,21 @@ class PlayPopupActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         appComponent.inject(this)
-    }
+        initLayout()
 
+    }
+    private fun initLayout(){
+
+        var dm = DisplayMetrics()
+        getWindowManager().getDefaultDisplay().getMetrics(dm)
+
+        var width = dm.widthPixels
+        var height = dm.heightPixels
+
+        getWindow().setLayout((width *0.8).roundToInt(), (height *0.6).roundToInt())
+
+        getWindow().setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))	// 배경화면 투명하게 하는 코드
+
+    }
     override fun fragment() = PlayPopupFragment()
 }
